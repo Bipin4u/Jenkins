6b17aefc831e544f805241ca225103766e2720ec
Bipin4u <bipinkumar9739340018@gmail.com>
2024-11-27 14:00:26 +0530

    push check

--- src/component/Greet.tsx
+++ src/component/Greet.tsx
@@ -3,7 +3,7 @@ interface nameProp {
 }
 const Greet:React.FC<nameProp> = ({name}) => {
   return (
-    <h1 style={{backgroundColor:"blue"}}>Greeting's {name}</h1>
+    <h1 style={{backgroundColor:"blue"}}>Greeting'sss {name}</h1>
   )
 }
 export default Greet
